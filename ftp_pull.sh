006235e403ab924f0adaa0a867293eea310cea06
Jeff Rule <jrule@omicia.com>
2014-01-05 22:51:20 -0800

    added new ftp pull process

---
+++ ftp_pull.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+
+export PGDATABASE=dbsnp
+
+PROG=$(basename $0)
+usage()
+{
+    echo "USAGE: $PROG --help --remote_path <remote_path> [--local_path <local path>] [--host host_name]"
+    echo ""
+    echo "  program to sync all remote files ending in .gz from remote site"
+    echo
+    echo "  The script will first sync all the *.md5 files"
+    echo "  for each *.md5 file, if the associated .gz file exists and has"
+    echo "  matches the .md5 it will be skipped, this allows for easy restart"
+    echo 
+    echo "  The list of tables to sync is derived from the list of synced *.md5 files"
+    echo 
+    echo "  --remote_path path on remote ftp site to pull all files"
+    echo "  --local_path directory to sync remote contents to"
+    echo "      If not specified, will default to a sub-directory"
+    echo "      with the same name as the last part of the remote path"
+    echo "  --host remote host"
+    echo "      IF not supplied defaults to ftp.ncbi.nlm.nih.gov"
+}
+
+usage_error()
+{
+    usage
+    echo "%%%-ERROR: you must supply a $1 parameter"
+    exit 1
+}
+
+
+remote_path=
+local_path=
+host=ftp.ncbi.nlm.nih.gov
+user=anonymous
+password=anonymous
+
+num_error=0
+num_skipped=0
+num_downloaded=0
+num_total=0
+
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        --he*)   # help
+            usage && exit 0
+            ;;
+        --r*) # remote_path
+            if [ $# -lt 2 ]; then
+                usage
+                echo "%%%-ERROR: you must supply and argument for $1 option"
+                exit1
+            fi
+            shift
+            remote_path=$1
+            ;;            
+         --l*) #local_path
+            if [ $# -lt 2 ]; then
+                usage
+                echo "%%%-ERROR: you must supply and argument for $1 option"
+                exit1
+            fi
+            shift
+            local_path="$1"
+            ;;
+         --ho*) #host
+            if [ $# -lt 2 ]; then
+                usage
+                echo "%%%-ERROR: you must supply and argument for $1 option"
+                exit1
+            fi
+            shift
+            host=$1
+            ;;             
+        *)
+            usage
+            echo "%%%-ERROR: unknown option ($1)"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+for param in remote_path
+do
+    remote_file=
+    if [ -z "${!param}" ]; then
+        usage_error -$param
+    fi
+done
+
+if [ -z "$local_path" ]; then
+    local_path=$(basename "$remote_path")
+    echo "%%%-INFO: no local path specified defaulting to $local_path"
+fi
+
+if [ ! -d "$local_path" ]; then
+    echo "%%%-INFO: local path ($local_path) does not exist, creating..."
+    mkdir "$local_path"
+    if [ $? -ne 0 ]; then
+        echo "%%%-ERROR: could not create local_path ($local_path), exiting..."
+    fi
+fi
+
+if [ ! -w "$local_path" ]; then
+    echo "%%%-ERROR: local directory ($local_path) is not writeable, exiting..."
+    exit 1
+fi
+
+if [ ! -r "$local_path" ]; then
+    echo "%%%-ERROR: local directory ($local_path) is not readable, exiting..."
+    exit 1
+fi
+
+if [ ! -x "$local_path" ]; then
+    echo "%%%-ERROR: local directory ($local_path) can not be listed, exiting..."
+    exit 1
+fi
+
+cd "$local_path"
+if [ $? -ne 0 ]; then
+    echo "%%%-ERROR: could not cd into local directory $local_path, exiting..."
+    exit 1
+fi
+
+ftp_path=ftp://anonymous:anonymous@$host
+
+get_md5()
+{
+    rm -f *.md5
+    CMD="(echo prompt; echo 'cd $remote_path'; echo 'mget *.md5') | ftp $ftp_path"
+    echo $CMD
+    eval $CMD
+    if [ $? -ne 0 ]; then
+        echo "    %%%-ERROR: problem getting list of *.md5 files from remote, exiting..."
+        exit 1
+    fi
+}
+
+#get_md5
+
+check_md5()
+{
+    local md5_file=$1
+    local file=$2
+    md5=$(md5 $file | perl -pi -e 's/\s+/\t/g' | cut -f4)
+    if [ $? -ne 0 ]; then
+        echo "    %%%-ERROR: problem getting md5 file $file, exiting..."
+        exit 1
+    fi
+    grep $md5 $md5_file > /dev/null 2>&1
+    return $?
+}
+
+ftp_get_file()
+{
+    local file=$1
+    CMD="ftp '$ftp_path${remote_path}/${file}'"
+    echo "    $CMD"
+    eval $CMD
+    if [ $? -ne 0 ]; then
+        echo "    %%%-ERROR: problem getting file (${remote_path}/${file}) from remote, exiting..."
+        return 1
+    fi 
+    return 0
+}
+
+for md5_file in *.md5
+do
+    num_total=$((num_total + 1))
+    echo "---------------------------------------------------------------------"
+    file=${md5_file%%.md5}
+    table=${file%%..bcp.gz}
+    echo "    %%%%-INFO: Processing files $file"
+    if [ -f $file ]; then
+        check_md5 $md5_file $file
+        if [ $? -eq 0 ]; then
+            num_skipped=$((num_skipped + 1))
+            echo "    %%%-INFO: file matches md5 stamp, skipping download..."
+            continue
+        fi
+    fi
+    ftp_get_file $file
+    if [ $? -ne 0 ]; then
+        num_error=$((num_error + 1))
+    else
+        num_downloaded=$((num_downloaded + 1))
+    fi
+done
+
+if [ $num_error -ne -0 ]; then
+    echo "%%%-ERROR: $num_error files failed to download"
+    echo "%%%-ERROR: total/error/skipped/downloaded $num_total/$num_error/$num_skipped/$num_downloaded"
+else
+    echo "%%%-SUCCESS:  total/error/skipped/downloaded $num_total/$num_error/$num_skipped/$num_downloaded"
+fi
+
--- pgsql_load.sh
+++ pgsql_load.sh
@@ -5,12 +5,41 @@ export PGDATABASE=dbsnp
 PROG=$(basename $0)
 usage()
 {
-    echo "USAGE: $PROG --help [--t table]"
-    echo "  --table  table to load"
+    echo "USAGE: $PROG --help --local_path <local_path_to_date> [--db dbname ] [--user db_user] [--pass db_pass] [--host db_host] [--reset] [--file <compressed bcp file>]"
+    echo "  --file          bcp.gz file to load (only loads a single table)"
+    echo "  --local_path    path to local data file directory"
+    echo "  --db            postgres database to load data into"
+    echo "  --user          postgres database user"
+    echo "  --pass          postgres database password"
+    echo "  --host          postgres database user"
+    echo "  --reset         clear the previously loaded flag for all files before loading"
+    echo ""
+    echo "  If no --table option is specified all tables in the specified --local_path"
+    echo "      directory are loaded, if and only if they match the md5 checksum and"
+    echo "      they have not been marked as loaded.   Each time a file is loaded a"
+    echo "      marker file called <file>.loaded is created in the same directory."
+    echo "      if this file exists, the table is not reloaded.   This allows for "
+    echo "      convient restarts and completion of partial loads"
 }
 
-file_list=*/*.gz
-table_name=
+usage_error()
+{
+    usage
+    echo "%%%-ERROR: you must supply a $1 parameter"
+    exit 1
+}
+
+file_list=*.gz
+num_error=0
+num_success=0
+num_total=0
+num_previous=0
+reset=
+local_path=
+db=
+dbuser=
+dbpass=
+dbhost=
 
 while [ $# -gt 0 ]
 do
@@ -18,15 +47,63 @@ do
         --he*)   # help
             usage && exit 0
             ;;
-        --t*able) #table
+        --f*) #file
             if [ $# -lt 2 ]; then
                 usage
-                echo "%%%-ERROR: you must supply and argument for $1 option"
+                echo "%%%-ERROR: you must supply an argument for $1 option"
                 exit1
             fi
             shift
-            table_name=$1
+            file_list=$1
             ;;            
+        --l*) #local_path
+            if [ $# -lt 2 ]; then
+                usage
+                echo "%%%-ERROR: you must supply an argument for $1 option"
+                exit1
+            fi
+            shift
+            local_path=$1
+            ;;  
+        --d*) #db
+            if [ $# -lt 2 ]; then
+                usage
+                echo "%%%-ERROR: you must supply an argument for $1 option"
+                exit1
+            fi
+            shift
+            db=$1
+            ;;  
+        --u*) #dbuser
+            if [ $# -lt 2 ]; then
+                usage
+                echo "%%%-ERROR: you must supply an argument for $1 option"
+                exit1
+            fi
+            shift
+            dbuser=$1
+            ;;  
+        --p*) #dbpass
+            if [ $# -lt 2 ]; then
+                usage
+                echo "%%%-ERROR: you must supply an argument for $1 option"
+                exit1
+            fi
+            shift
+            dbpass=$1
+            ;;  
+        --h*) #dbhost
+            if [ $# -lt 2 ]; then
+                usage
+                echo "%%%-ERROR: you must supply an argument for $1 option"
+                exit1
+            fi
+            shift
+            dbhost=$1
+            ;;  
+        --r*) #reset
+            reset=true
+            ;; 
         *)
             usage
             echo "%%%-ERROR: unknown option ($1)"
@@ -36,42 +113,133 @@ do
     shift
 done
 
-if [ ! -z "$table_name" ]; then
-    file_list=*/${table_name}.bcp.gz
+for param in local_path 
+do
+    remote_file=
+    if [ -z "${!param}" ]; then
+        usage_error -$param
+    fi
+done
+
+[ ! -z $db ] && export PGDATABASE=$db
+[ ! -z $dbuser ] && export PGUSER=$dbuser
+[ ! -z $dbpass ] && export PGPASSWORD=$dbpass
+[ ! -z $dbhost ] && export PGHOST=$dbhost
+
+psql -q -w < /dev/null
+if [ $? -ne 0 ]; then
+    echo "%%%-ERROR: can not connect to database, exiting"
+    exit 1
 fi
 
-for f in $file_list
+if [ ! -r "$local_path" ]; then
+    echo "%%%-ERROR: local directory ($local_path) is not readable, exiting..."
+    exit 1
+fi
+
+if [ ! -x "$local_path" ]; then
+    echo "%%%-ERROR: local directory ($local_path) can not be listed, exiting..."
+    exit 1
+fi
+
+if  [ ! -z $reset ]; then
+    rm -f $local_path/*.loaded
+    if [ $? -ne 0 ]; then
+        echo "%%%-ERROR: problem clearing list of loaded files"
+        exit 1
+    fi
+    echo "%%%-INFO: all previous load flags cleared for tables in $local_path"
+fi
+
+for f in $(echo "$local_path/$file_list")
 do
+    echo 
+    echo ----------------------------------------------------------------------------------------------------------------------------------------------
+    num_total=$((num_total + 1))
     echo
-    echo "%%%-INFO: $(date) starting to load table $f -------------------------------------"
+    echo "    %%%-INFO: $(date) starting to load table $f "
+    
+    if [ -f $f.loaded ]; then
+        num_previous=$((num_previous + 1))
+        echo "    %%%-INFO: file already loaded, skipping"
+        continue
+    fi
+    
+    if [ ! -r $f ]; then
+        num_error=$((num_error + 1))
+        echo "    %%%-ERROR: can't read file $f, skipping"
+        continue;
+    fi
+    
     md5_file=$f.md5
     if [ ! -f $md5_file ]; then
-        echo "%%%-ERROR: can't find md5 file $md5_file, exiting..."
-        exit 1
+        num_error=$((num_error + 1))
+        echo "    %%%-ERROR: can't find md5 file $md5_file, exiting..."
+        continue
     fi
-    md5=$(md5 $f | perl -pi -e 's/\s+/\t/g' | cut -f4)
+    
+    if [ ! -r $md5_file ]; then
+        num_error=$((num_error + 1))
+        echo "    %%%-ERROR: md5 stamp file not readable, skiplling..."
+        continue
+    fi
+    
+    md5=$(md5 $f 2>/dev/null | perl -pi -e 's/\s+/\t/g' | cut -f4)
     grep "$md5" $md5_file > /dev/null 2>&1
     if [ $? -ne 0 ]; then
-        echo "%%%-ERROR: md5 stamp $md5 does not match $(cat $md5_file), exiting..."
-        exit 1
+        num_error=$((num_error + 1))
+        echo "    %%%-ERROR: md5 stamp $md5 does not match stored: ($(cat $md5_file)), skipping..."
+        continue
     fi
+    
     base_file=$(basename $f)
     table_name=${base_file%%.bcp.gz}
     
-    echo "    %%%-INFO: starting load into table $table_name"
+
+    echo "    %%%-INFO: loading $(gzcat $f | wc -l) lines of data from a $(du -sh $f|cut -f1) compressed file..."
+
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     gzcat $f | head -5
-    psql --echo-all -c "truncate table $table_name"
+    CMD="psql -c \"truncate table $table_name\""
+    echo $CMD
+    eval $CMD
     if [ $? -ne 0 ]; then
-        echo "%%%-ERROR: could not truncate table $table_name, exiting"
-        exit 1
+        num_error=$((num_error + 1))
+        echo "    %%%-ERROR: could not truncate table $table_name, skipping"
+        continue
     fi
-    gzcat $f | psql --echo-all -c "copy $table_name from STDIN" 
+    CMD="(echo '\timing on'; echo \"copy $table_name from STDIN with (null '');\"; gzcat $f; echo '\.') | psql -q"
+    echo $CMD
+    eval $CMD
     if [ $? -ne 0 ]; then
-        echo "%%%-ERROR could not load table $table_name, exiting"
-        exit 1
+        num_error=$((num_error + 1))
+        echo "    %%%-ERROR could not load table $table_name, skipping"
+        continue
     fi
+    touch $f.loaded
+    if [ ! -f $f.loaded ]; then
+        num_error=$((num_error + 1))
+        echo "    %%%-ERROR: problem touching $f.loaded"
+        continue
+    fi
+    
     psql --echo-all -c "select count(*) from $table_name"
     psql --echo-all -c "select * from $table_name limit 5"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo 
+
+    num_success=$((num_success + 1))
+    echo "    %%%-INFO: $(date) COMPLETE load table $f "
+
 done
+
+echo
+echo
+if [ $num_error -gt 0 ]; then
+    echo "%%%-ERROR: $num_error files not loaded"
+    echo "%%%-ERROR: total/already_loaded/loaded/error files $num_total/$num_previous/$num_success/$num_error"
+else
+    echo "%%%-SUCCESS: total/already_loaded/loaded/error files $num_total/$num_previous/$num_success/$num_error"
+fi
 
     
